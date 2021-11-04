e443843bf81cbe91e78854e8296737a6c1e9f0da
fffnite <bradley.whiteford@live.co.uk>
2021-11-04 14:58:43 +0000

    Add ConvertFrom-K12 function and update help

--- ps-oneroster.psm1
+++ ps-oneroster.psm1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Connects to a oneroster api and sets an environment variable with the API token 
+    Connects to a oneroster api and sets an environment variable with the API token
 .EXAMPLE
     PS C:\> Connect-Oneroster -Domain 'www.myserver.com' -ClientId 'abcedf' -ClientSecret 'superSecret'
     Gets an API token from the oneroster server at www.myserver.com/ims/oneroster/v1p1
@@ -158,6 +158,14 @@ function Get-Data {
 
 }
 
+<#
+.SYNOPSIS
+    Uses sql style joins to join classes to enrollments and courses to those classes
+.EXAMPLE
+    PS C:\> Get-EnrollmentsJoined
+.OUTPUTS
+    PSCustomObject[]
+#>
 function Get-EnrollmentsJoined {
 
     [CmdletBinding()]
@@ -236,4 +244,52 @@ function Get-EnrollmentsJoined {
 
     return $output
     
+}
+
+<#
+.SYNOPSIS
+    Provides a lookup table to convert CEDS into closest other schooling year
+    group standard and provides a index for logical operations
+.EXAMPLE
+    PS C:\> ConvertFrom-K12 -Year PK -To SCO
+.OUTPUTS
+    PSCustomObject[]
+#>
+function ConvertFrom-K12 {
+    param (
+        [int]$index,
+        [string]$Year,
+        [string]$To,
+        [switch]$All,
+        [switch]$ToIndex
+    )
+
+    begin {
+        $t = @(
+            [pscustomobject]@{ "INDEX" = 0; "CEDS" = "IT"; "SCO" = ""; "ENG" = ""; }
+            [pscustomobject]@{ "INDEX" = 1; "CEDS" = "PR"; "SCO" = "" ; "ENG" = ""; }
+            [pscustomobject]@{ "INDEX" = 2; "CEDS" = "PK"; "SCO" = "N"; "ENG" = ""; }
+            [pscustomobject]@{ "INDEX" = 3; "CEDS" = "TK"; "SCO" = "KN"; "ENG" = "Reception"; }
+            [pscustomobject]@{ "INDEX" = 4; "CEDS" = "KG"; "SCO" = "P1"; "ENG" = "1"; }
+            [pscustomobject]@{ "INDEX" = 5; "CEDS" = "01"; "SCO" = "P2"; "ENG" = "2"; }
+            [pscustomobject]@{ "INDEX" = 6; "CEDS" = "02"; "SCO" = "P3"; "ENG" = "3"; }
+            [pscustomobject]@{ "INDEX" = 7; "CEDS" = "03"; "SCO" = "P4"; "ENG" = "4"; }
+            [pscustomobject]@{ "INDEX" = 8; "CEDS" = "04"; "SCO" = "P5"; "ENG" = "5"; }
+            [pscustomobject]@{ "INDEX" = 9; "CEDS" = "05"; "SCO" = "P6"; "ENG" = "6"; }
+            [pscustomobject]@{ "INDEX" = 10; "CEDS" = "06"; "SCO" = "P7"; "ENG" = "7"; }
+            [pscustomobject]@{ "INDEX" = 11; "CEDS" = "07"; "SCO" = "S1"; "ENG" = "8"; }
+            [pscustomobject]@{ "INDEX" = 12; "CEDS" = "08"; "SCO" = "S2"; "ENG" = "9"; }
+            [pscustomobject]@{ "INDEX" = 13; "CEDS" = "09"; "SCO" = "S3"; "ENG" = "10"; }
+            [pscustomobject]@{ "INDEX" = 14; "CEDS" = "10"; "SCO" = "S4"; "ENG" = "11"; }
+            [pscustomobject]@{ "INDEX" = 15; "CEDS" = "11"; "SCO" = "S5"; "ENG" = "12"; }
+            [pscustomobject]@{ "INDEX" = 16; "CEDS" = "12"; "SCO" = "S6"; "ENG" = "13"; }
+        )
+    }
+
+    process {
+        if ($index) {$t | where-object {$_.index -eq $index}}
+        if ($to) {$t | where-object {$_.CEDS -eq $year} | Select-Object index,$to}
+        if ($all) {$t}
+        if ($toIndex) {($t | where-object {$_.CEDS -eq $year} | Select-Object index).INDEX}
+    }
 }
